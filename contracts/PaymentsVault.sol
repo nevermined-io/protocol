71e6225f7f5390a651566fa64f0da2e6c48c4739
enrique <enrique@nevermined.io>
2025-03-19 18:03:35 +0100

    feat: use same prettier config that rest of projects

--- contracts/PaymentsVault.sol
+++ contracts/PaymentsVault.sol
@@ -3,23 +3,21 @@
 // Code is Apache-2.0 and docs are CC-BY-4.0
 pragma solidity ^0.8.28;
 
-import {Initializable} from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
-import {INVMConfig} from './interfaces/INVMConfig.sol';
-import {IVault} from './interfaces/IVault.sol';
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import {ReentrancyGuardUpgradeable} from '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
-
+import { Initializable } from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import { INVMConfig } from './interfaces/INVMConfig.sol';
+import { IVault } from './interfaces/IVault.sol';
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { ReentrancyGuardUpgradeable } from '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
 
 contract PaymentsVault is Initializable, IVault, ReentrancyGuardUpgradeable {
-
   /**
    * @notice Role allowing to deposit assets into the Vault
-  */
+   */
   bytes32 public constant DEPOSITOR_ROLE = keccak256('VAULT_DEPOSITOR_ROLE');
 
   /**
    * @notice Role allowing to withdraw assets from the Vault
-  */
+   */
   bytes32 public constant WITHDRAW_ROLE = keccak256('VAULT_WITHDRAW_ROLE');
 
   INVMConfig internal nvmConfig;
@@ -27,39 +25,28 @@ contract PaymentsVault is Initializable, IVault, ReentrancyGuardUpgradeable {
   /// Only an account with the right role can access this function
   /// @param sender The address of the account calling this function
   /// @param role The role required to call this function
-  error InvalidRole(address sender, bytes32 role);  
+  error InvalidRole(address sender, bytes32 role);
 
   /// Error sending native token (i.e ETH)
   error FailedToSendNativeToken();
 
-  event ReceivedNativeToken(
-    address indexed from, 
-    uint256 value
-  );
+  event ReceivedNativeToken(address indexed from, uint256 value);
 
-  event WithdrawNativeToken(    
-    address indexed from, 
-    address indexed receiver,
-    uint256 amount
-  );
+  event WithdrawNativeToken(address indexed from, address indexed receiver, uint256 amount);
 
-  event ReceivedERC20(
-    address indexed erc20TokenAddress,
-    address indexed from, 
-    uint256 amount
-  );
+  event ReceivedERC20(address indexed erc20TokenAddress, address indexed from, uint256 amount);
 
   event WithdrawERC20(
     address indexed erc20TokenAddress,
-    address indexed from, 
+    address indexed from,
     address indexed receiver,
     uint256 amount
   );
 
   function initialize(address _nvmConfigAddress) public initializer {
-    ReentrancyGuardUpgradeable.__ReentrancyGuard_init();    
+    ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
     nvmConfig = INVMConfig(_nvmConfigAddress);
-  }  
+  }
 
   receive() external payable {
     if (!nvmConfig.hasRole(msg.sender, DEPOSITOR_ROLE))
@@ -67,45 +54,46 @@ contract PaymentsVault is Initializable, IVault, ReentrancyGuardUpgradeable {
     emit ReceivedNativeToken(msg.sender, msg.value);
   }
 
-  function depositNativeToken() 
-  external payable nonReentrant
-  {
+  function depositNativeToken() external payable nonReentrant {
     if (!nvmConfig.hasRole(msg.sender, DEPOSITOR_ROLE))
       revert InvalidRole(msg.sender, DEPOSITOR_ROLE);
     emit ReceivedNativeToken(msg.sender, msg.value);
   }
 
-  function withdrawNativeToken(uint256 _amount, address _receiver) 
-  external nonReentrant  
-  {
-    if (!nvmConfig.hasRole(msg.sender, WITHDRAW_ROLE)) revert InvalidRole(msg.sender, WITHDRAW_ROLE);
-    
+  function withdrawNativeToken(uint256 _amount, address _receiver) external nonReentrant {
+    if (!nvmConfig.hasRole(msg.sender, WITHDRAW_ROLE))
+      revert InvalidRole(msg.sender, WITHDRAW_ROLE);
+
     // Emit event before external call to follow checks-effects-interactions pattern
     emit WithdrawNativeToken(msg.sender, _receiver, _amount);
-    
-    (bool sent, ) = _receiver.call{value: _amount}('');
+
+    (bool sent, ) = _receiver.call{ value: _amount }('');
     if (!sent) revert FailedToSendNativeToken();
   }
 
-  function depositERC20(address _erc20TokenAddress, uint256 _amount, address _from) 
-  external nonReentrant 
-  {
+  function depositERC20(
+    address _erc20TokenAddress,
+    uint256 _amount,
+    address _from
+  ) external nonReentrant {
     if (!nvmConfig.hasRole(msg.sender, DEPOSITOR_ROLE))
       revert InvalidRole(msg.sender, DEPOSITOR_ROLE);
-    emit ReceivedERC20(_erc20TokenAddress, _from, _amount);    
+    emit ReceivedERC20(_erc20TokenAddress, _from, _amount);
   }
 
-  function withdrawERC20(address _erc20TokenAddress, uint256 _amount, address _receiver) 
-  external nonReentrant  
-  {
+  function withdrawERC20(
+    address _erc20TokenAddress,
+    uint256 _amount,
+    address _receiver
+  ) external nonReentrant {
     if (!nvmConfig.hasRole(msg.sender, WITHDRAW_ROLE))
       revert InvalidRole(msg.sender, WITHDRAW_ROLE);
-    
+
     IERC20 token = IERC20(_erc20TokenAddress);
     // Use transfer instead of transferFrom since we're sending from our own balance
     token.transfer(_receiver, _amount);
-    
-    emit WithdrawERC20(_erc20TokenAddress, msg.sender, _receiver, _amount);    
+
+    emit WithdrawERC20(_erc20TokenAddress, msg.sender, _receiver, _amount);
   }
 
   function getBalanceNativeToken() external view returns (uint256 balance) {
@@ -114,7 +102,7 @@ contract PaymentsVault is Initializable, IVault, ReentrancyGuardUpgradeable {
 
   function getBalanceERC20(address _erc20TokenAddress) external view returns (uint256 balance) {
     IERC20 token = IERC20(_erc20TokenAddress);
-    return token.balanceOf(address(this));    
+    return token.balanceOf(address(this));
   }
 
 }
